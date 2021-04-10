a74252aaa3b60b047e5446ffc782dac7a8ef4d0f
engine-oper <smxblysq@qq.com>
2021-04-10 14:30:14 +0800

    初次推送代码

--- lib/QcloudCos.py
+++ lib/QcloudCos.py
@@ -99,8 +99,12 @@ class CosUpload:
             RecodeLog.info(msg="{0},{1},{2},三者信息对应，检查无问题！".format(*[os.path.basename(x) for x in achieve_list]))
             return achieve_list
         else:
-            RecodeLog.error(msg="{0},{1},{2},三者信息不对应对应，检查不通过，请打包人员检查！".format(*[os.path.basename(x) for x in achieve_list]))
-            self.alert(message="{0},{1},{2},三者信息不对应对应，检查不通过，请打包人员检查！".format(*[os.path.basename(x) for x in achieve_list]))
+            RecodeLog.error(msg="{0},{1},{2},三者信息不对应对应，检查不通过，请打包人员检查！".format(
+                *[os.path.basename(x) for x in achieve_list]
+            ))
+            self.alert(message="{0},{1},{2},三者信息不对应对应，检查不通过，请打包人员检查！".format(
+                *[os.path.basename(x) for x in achieve_list]
+            ))
             return False
 
     def check_url(self, url_list):
